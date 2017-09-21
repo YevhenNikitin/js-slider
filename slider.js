50b26e93b31dca8e5ddb128cb6a21eb3ccbf824d
YevhenNikitin <noik60@gmail.com>
2017-09-21 17:38:25 +0300

    Comments added

--- slider.js
+++ slider.js
@@ -1,6 +1,6 @@
-var slides = document.querySelectorAll('#slides .slide');
-var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,2000);
+var slides = document.querySelectorAll('#slides .slide'); // get all slides from the container
+var currentSlide = 0; // create a variable to get the current slide
+var slideInterval = setInterval(nextSlide,2000); // set 2-seconds-interval for the next slide
 
 function nextSlide() {
  slides[currentSlide].className = 'slide';
